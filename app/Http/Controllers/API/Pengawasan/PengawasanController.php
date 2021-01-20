58d6e0f61911b6f1e36f7b3de44b16f3012ec740
wahyu safaat <wahyusafaat0506@gmail.com>
2021-01-20 12:00:33 +0700

    fix error for pengawasan API
    memperbaiki error pada get_all data, dan update sehingga api sudah berjalan baik CRUD

--- app/Http/Controllers/API/Pengawasan/PengawasanController.php
+++ app/Http/Controllers/API/Pengawasan/PengawasanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\Pengawasan;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Pengawasan;
+use Illuminate\Support\Facades\DB;
 
 class PengawasanController extends Controller
 {
@@ -20,37 +21,38 @@ class PengawasanController extends Controller
     public function store(Request $request)
     {
         $validateData = $request->validate([
-            'pekerjaan' => 'required',
-            'pelaksana_pekerjaan' => 'required',
-            'no_kontrak' => 'required',
-            'tanggal_pekerjaan' => 'required',
-            'lokasi_pekerjaan' => 'required',
-            'pengawas_pln' => 'required',
-            'pengawas_vendor' => 'required',
+            'pekerjaan'             => 'required',
+            'pelaksana_pekerjaan'   => 'required',
+            'no_kontrak'            => 'required',
+            'tanggal_pekerjaan'     => 'required',
+            'lokasi_pekerjaan'      => 'required',
+            'pengawas_pln'          => 'required',
+            'pengawas_vendor'       => 'required',
             'jml_petugas_pelaksana' => 'required',
-            'sop' => 'required',
-            'IBPR' => 'required',
-            'JSA' => 'required',
-            'working_permit' => 'required',
-            'arahan_pekerja' => 'required',
-            'cek_komunikasi' => 'required'
+            'sop'                   => 'required',
+            'IBPR'                  => 'required',
+            'JSA'                   => 'required',
+            'working_permit'        => 'required',
+            'arahan_pekerja'        => 'required',
+            'cek_komunikasi'        => 'required'
         ]);
         
         $dataPengawasan = new Pengawasan;
-        $dataPengawasan->pekerjaan = $request->pekerjaan;
-        $dataPengawasan->pelaksana_pekerjaan = $request->pelaksana_pekerjaan;
-        $dataPengawasan->no_kontrak = $request->no_kontrak;
-        $dataPengawasan->tanggal_pekerjaan = $request->tanggal_pekerjaan;
-        $dataPengawasan->lokasi_pekerjaan = $request->lokasi_pekerjaan;
-        $dataPengawasan->pengawas_pln = $request->pengawas_pln;
-        $dataPengawasan->pengawas_vendor = $request->pengawas_vendor;
-        $dataPengawasan->jml_petugas_pelaksana = $request->jml_petugas_pelaksana;
-        $dataPengawasan->sop = $request->sop;
-        $dataPengawasan->IBPR = $request->IBPR;
-        $dataPengawasan->JSA = $request->JSA;
-        $dataPengawasan->working_permit = $request->working_permit;
-        $dataPengawasan->arahan_pekerja = $request->arahan_pekerja;
-        $dataPengawasan->cek_komunikasi = $request->cek_komunikasi;
+        $dataPengawasan->id                     = $request->id;
+        $dataPengawasan->pekerjaan              = $request->pekerjaan;
+        $dataPengawasan->pelaksana_pekerjaan    = $request->pelaksana_pekerjaan;
+        $dataPengawasan->no_kontrak             = $request->no_kontrak;
+        $dataPengawasan->tanggal_pekerjaan      = $request->tanggal_pekerjaan;
+        $dataPengawasan->lokasi_pekerjaan       = $request->lokasi_pekerjaan;
+        $dataPengawasan->pengawas_pln           = $request->pengawas_pln;
+        $dataPengawasan->pengawas_vendor        = $request->pengawas_vendor;
+        $dataPengawasan->jml_petugas_pelaksana  = $request->jml_petugas_pelaksana;
+        $dataPengawasan->sop                    = $request->sop;
+        $dataPengawasan->IBPR                   = $request->IBPR;
+        $dataPengawasan->JSA                    = $request->JSA;
+        $dataPengawasan->working_permit         = $request->working_permit;
+        $dataPengawasan->arahan_pekerja         = $request->arahan_pekerja;
+        $dataPengawasan->cek_komunikasi         = $request->cek_komunikasi;
         $dataPengawasan->save();
 
         return response()->json($dataPengawasan, 201);
@@ -59,37 +61,39 @@ class PengawasanController extends Controller
     public function update(Request $request)
     {
         $validateData = $request->validate([
-            'pekerjaan' => 'required',
-            'pelaksana_pekerjaan' => 'required',
-            'no_kontrak' => 'required',
-            'tanggal_pekerjaan' => 'required',
-            'lokasi_pekerjaan' => 'required',
-            'pengawas_pln' => 'required',
-            'pengawas_vendor' => 'required',
+            'id'                    => 'required',
+            'pekerjaan'             => 'required',
+            'pelaksana_pekerjaan'   => 'required',
+            'no_kontrak'            => 'required',
+            'tanggal_pekerjaan'     => 'required',
+            'lokasi_pekerjaan'      => 'required',
+            'pengawas_pln'          => 'required',
+            'pengawas_vendor'       => 'required',
             'jml_petugas_pelaksana' => 'required',
-            'sop' => 'required',
-            'IBPR' => 'required',
-            'JSA' => 'required',
-            'working_permit' => 'required',
-            'arahan_pekerja' => 'required',
-            'cek_komunikasi' => 'required'
+            'sop'                   => 'required',
+            'IBPR'                  => 'required',
+            'JSA'                   => 'required',
+            'working_permit'        => 'required',
+            'arahan_pekerja'        => 'required',
+            'cek_komunikasi'        => 'required'
         ]);
         
         $dataPengawasan = Pengawasan::where('id','=',$request->id)->first();
-        $dataPengawasan->pekerjaan = $request->pekerjaan;
-        $dataPengawasan->pelaksana_pekerjaan = $request->pelaksana_pekerjaan;
-        $dataPengawasan->no_kontrak = $request->no_kontrak;
-        $dataPengawasan->tanggal_pekerjaan = $request->tanggal_pekerjaan;
-        $dataPengawasan->lokasi_pekerjaan = $request->lokasi_pekerjaan;
-        $dataPengawasan->pengawas_pln = $request->pengawas_pln;
-        $dataPengawasan->pengawas_vendor = $request->pengawas_vendor;
-        $dataPengawasan->jml_petugas_pelaksana = $request->jml_petugas_pelaksana;
-        $dataPengawasan->sop = $request->sop;
-        $dataPengawasan->IBPR = $request->IBPR;
-        $dataPengawasan->JSA = $request->JSA;
-        $dataPengawasan->working_permit = $request->working_permit;
-        $dataPengawasan->arahan_pekerja = $request->arahan_pekerja;
-        $dataPengawasan->cek_komunikasi = $request->cek_komunikasi;
+        $dataPengawasan->id                     = $request->id;
+        $dataPengawasan->pekerjaan              = $request->pekerjaan;
+        $dataPengawasan->pelaksana_pekerjaan    = $request->pelaksana_pekerjaan;
+        $dataPengawasan->no_kontrak             = $request->no_kontrak;
+        $dataPengawasan->tanggal_pekerjaan      = $request->tanggal_pekerjaan;
+        $dataPengawasan->lokasi_pekerjaan       = $request->lokasi_pekerjaan;
+        $dataPengawasan->pengawas_pln           = $request->pengawas_pln;
+        $dataPengawasan->pengawas_vendor        = $request->pengawas_vendor;
+        $dataPengawasan->jml_petugas_pelaksana  = $request->jml_petugas_pelaksana;
+        $dataPengawasan->sop                    = $request->sop;
+        $dataPengawasan->IBPR                   = $request->IBPR;
+        $dataPengawasan->JSA                    = $request->JSA;
+        $dataPengawasan->working_permit         = $request->working_permit;
+        $dataPengawasan->arahan_pekerja         = $request->arahan_pekerja;
+        $dataPengawasan->cek_komunikasi         = $request->cek_komunikasi;
         $dataPengawasan->save();
 
         return response()->json($dataPengawasan, 201);
